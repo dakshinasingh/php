4a68be2c61540639feae9779d7c8c8cf6384505d
Dakshina <dakshinasingh25@gmail.com>
2023-01-04 18:44:50 +0530

    lec3

---
+++ variable.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    echo"heyy";
+    echo"<br>";
+    
+    //its a comment
+    $var1="dakshina";
+    echo $var1;
+    echo"<br>";
+    $x = 5;
+    $y = 10;
+    echo $x;
+    echo"<br>";
+    echo $y;
+    echo"<br>";
+    echo "first variable is $x";
+    echo "<br>";
+    echo "second variable is $y";
+    echo "<br>";
+    $sum=$x+$y;
+    echo "The sum of both variables is $sum";
+    echo "<br>";
+    ?>
+</body>
+</html>
