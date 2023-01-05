7db8956a3cdfdf2ec10e8d966af10b640ee21b31
Dakshina <dakshinasingh25@gmail.com>
2023-01-05 15:00:41 +0530

    string functions

---
+++ strings.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Strings</title>
+    </head>
+    <body>
+
+        <?php
+        
+        echo str_word_count("Hello world!");//counts the words in string
+        echo "<hr />";
+
+
+        echo strrev("Hello world!"); // reverse the string
+        echo "<hr />";
+
+
+        echo strpos("Hello world!", "world"); // outputs the position of text in the string
+        echo "<hr />";
+
+
+        echo str_replace("world", "John", "Hello world!"); // replaces text inside the string
+        //replaces world with john in hello world....output- hello john
+        echo "<hr />";
+        ?>
+
+    </body>
+</html>
