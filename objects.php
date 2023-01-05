7cb978498aa00032ffb845620c0a80f7dc5b8b09
Dakshina <dakshinasingh25@gmail.com>
2023-01-05 13:28:37 +0530

    objects

--- objects.php
+++ objects.php
@@ -1,9 +1,31 @@
 <!DOCTYPE html>
 <html>
+    <head>
+      <title> objects in PHP </title>
+    </head>
     <body>
 
         <?php
         echo "<h1>objects in PHP</h1>";
+        class Person{
+            public $fname;
+            public $lname;
+            public $age;
+            public function __construct($fname, $lname, $age){
+                $this->fname = $fname;
+                $this->lname = $lname;
+                $this->age = $age;
+            }
+            public function hello(){
+                return "I am " . $this->fname . " ". $this->lname . " and my age is " . $this->age . "";       
+            }
+        }
+            $person1 = new Person('Dakshina', 'Singh' , 22);
+            $person2 = new Person('Daksh', 'Singh', 18);
+            echo $person1->hello();
+            echo "<br>";
+            echo $person2->hello();
+
         ?>
 
     </body>
